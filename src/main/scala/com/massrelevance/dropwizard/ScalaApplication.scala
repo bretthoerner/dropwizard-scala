ff7b2d9df90ba2307fa61203dea95438aaf4928c
Brett Hoerner <brett@bretthoerner.com>
2016-08-16 12:58:23 -0500

    Point at https://github.com/datasift/dropwizard-scala

--- src/main/scala/com/massrelevance/dropwizard/ScalaApplication.scala
+++
@@ -1,9 +0,0 @@
-package com.massrelevance.dropwizard
-
-import io.dropwizard.{Application, Configuration}
-
-abstract class ScalaApplication[T <: Configuration] extends Application[T] {
-  final def main(args: Array[String]) {
-    run(args.toArray: _*)
-  }
-}
